8c6d3418c250173892b2e13247fb6194b6f55e97
John <john.19col@gmail.com>
2019-12-15 21:45:34 -0500

    Add sample contactInfo and person structs

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName string
+	contactInfo contactInfo
+}
+
+func main() {
+	person := person{
+		firstName: "Michael",
+		lastName: "Jackson",
+		contactInfo: contactInfo{
+			email: "michael@mail.com",
+		},
+	}
+
+	fmt.Printf("%+v\n", person)
+}
